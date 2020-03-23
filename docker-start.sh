bfbb160deb915f1f08ca4807225ccc9f8c7f0179
aogg <524383495@qq.com>
2020-03-23 22:27:14 +0800

    优化初始化 1

--- docker-start.sh
+++ docker-start.sh
@@ -2,15 +2,15 @@
 
 volume_root="/data"
 volume_conf="${volume_root}/conf"
-app_conf="/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/META-INF/conf/"
+app_conf="/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/META-INF/"
 # 处理首次共享目录
 if ! test -d $volume_conf; then
     mkdir -p $volume_conf
-    cp -r $app_conf $volume_root
+    cp -r "${app_conf}/conf/" $volume_root
     
-    rm -Rf $app_conf/*
-    ln -s $volume_conf $app_conf
 fi
 
+rm -Rf "${app_conf}/conf"
+ln -s $volume_conf $app_conf
 
 exec "$@"
