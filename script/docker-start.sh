9764c6b673c1b3d9699a55ab445b2623c6cd3a5f
aogg <aogg@users.noreply.github.com>
2022-03-06 21:27:59 +0800

    Update docker-start.sh

--- script/docker-start.sh
+++ script/docker-start.sh
@@ -2,9 +2,13 @@
 
 volume_root="/tmp-finderweb-data"
 volume_conf="${volume_root}/conf"
-app_conf="/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/META-INF"
+
+# 2.5.5
+# app_conf="/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/META-INF"
+
+app_conf="/usr/local/tomcat/webapps/ROOT/WEB-INF/finderweb"
 # 处理首次共享目录
-if [ ! -d "${app_conf}/conf/user/" ]; then
+if [ ! -d "${app_conf}/user/" ]; then
     cp -r "${volume_conf}/" "${app_conf}/"
 fi
 
