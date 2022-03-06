81490fa743b2d68de15f08e65d1f0dc7f535928b
aogg <aogg@users.noreply.github.com>
2022-03-06 21:53:05 +0800

    Update docker-start.sh

--- script/docker-start.sh
+++ script/docker-start.sh
@@ -9,7 +9,7 @@ volume_conf="${volume_root}/finderweb"
 app_conf="/usr/local/tomcat/webapps/ROOT/WEB-INF/finderweb"
 # 处理首次共享目录
 if [ ! -d "${app_conf}/user/" ]; then
-    cp -r "${volume_conf}/" "${app_conf}/"
+    cp -r "${volume_conf}/*" "${app_conf}/"
 fi
 
 exec "$@"
