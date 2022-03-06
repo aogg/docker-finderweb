367436e46a6dafe6cbe781223c420ef430620d91
aogg <aogg@users.noreply.github.com>
2022-03-06 21:17:35 +0800

    Update docker-start.sh

--- script/docker-start.sh
+++ script/docker-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-volume_root="/data"
+volume_root="/tmp-finderweb-data"
 volume_conf="${volume_root}/conf"
 app_conf="/usr/local/tomcat/webapps/ROOT/WEB-INF/classes/META-INF"
 # 处理首次共享目录
